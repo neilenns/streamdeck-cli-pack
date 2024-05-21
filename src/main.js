46a1154edfd45479519d61a797a5a2d11014304b
Neil <neile@live.com>
2024-05-21 10:58:58 -0700

    Try auto-calculating version

--- src/main.js
+++ src/main.js
@@ -15,13 +15,20 @@ async function run() {
     arguments.push('--force')
   }
 
-  if (core.getInput('version')) {
-    arguments.push('--version', core.getInput('version'))
+  const version = getVersion()
+
+  if (version) {
+    arguments.push('--version', version)
   }
 
   await exec.exec('npx', arguments)
 }
 
+/**
+ * Figures out the location of the plugin directory. Uses the provided `path` input if it is set, otherwise
+ * looks for a directory ending in `.sdPlugin` in the current working directory.
+ * @returns {string} The path to the plugin directory
+ */
 function getSdPluginPath() {
   let sdPluginPath = core.getInput('path')
 
@@ -49,6 +56,49 @@ function getSdPluginPath() {
 
   return sdPluginPath
 }
+
+/**
+ * Figures out the version for the plugin. Uses the provided `version` input if it is set, otherwise
+ * uses either the github tag version or the pull request number.
+ * @returns {string} The version string
+ */
+function getVersion() {
+  let version = core.getInput('version')
+
+  if (version !== '') {
+    return version
+  }
+
+  // Version wasn't specified so try getting something from the github environment
+  const githubRef = process.env.GITHUB_REF
+
+  if (!githubRef) {
+    core.warn('No github ref found, using manifest.json version')
+    return undefined
+  }
+
+  // Try getting the version from the release tag
+  if (githubgithubRef.startsWith('refs/tags/')) {
+    version = `${githubRef.replace('refs/tags/', '').replace('v', '')}.0`
+    core.info(`Using github tag version '${version}'`)
+
+    return version
+  }
+
+  // Try getting the version from the pull request number
+  if (githubRef.startsWith('refs/pull/')) {
+    version = `0.0.${githubRef.replace('refs/pull/', '')}.0`
+    core.info(`Using github pull request number '${version}'`)
+
+    return version
+  }
+
+  core.warn(
+    'No version found in github environment, using manifest.json version'
+  )
+  return undefined
+}
+
 module.exports = {
   run
 }
